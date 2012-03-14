d29123298507a402117a6c11d3bd691c4e971d98
Jakub Matraszek <jakubm@innov8on.com>
2012-03-14 11:40:20 +0100

    added mailcap and auto view text/html

--- install.rb
+++ install.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 
+# REQUIREMENTS
+# lynx, urlview
+
+
 require 'fileutils'
 include FileUtils
 
 ["gitconfig", "muttrc"].each { |file| cp "#{file}.example", file }
 
-["ackrc", "bash_aliases", "bashrc", "gitconfig", "irbrc", "mutt", "muttrc", "tigrc"].each do |file|
+["ackrc", "bash_aliases", "bashrc", "gitconfig", "irbrc", "mailcap", "mutt", "muttrc", "tigrc"].each do |file|
   ln_sf file, "~/.#{file}"
 end
