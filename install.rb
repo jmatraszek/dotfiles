31e5cbeb6d7502492240eb0f5661b2e4c778c0e9
Jakub Matraszek <jakubm@innov8on.com>
2012-04-17 15:25:26 +0200

    install tmux when running install.rb

--- install.rb
+++ install.rb
@@ -9,6 +9,6 @@ include FileUtils
 
 ["gitconfig", "muttrc"].each { |file| cp "#{file}.example", file }
 
-["ackrc", "bash_aliases", "bashrc", "gitconfig", "irbrc", "irssi", "mailcap", "mutt", "muttrc", "tigrc"].each do |file|
+["ackrc", "bash_aliases", "bashrc", "gitconfig", "irbrc", "irssi", "mailcap", "mutt", "muttrc", "tigrc", "tmux.conf"].each do |file|
   ln_sf file, "~/.#{file}"
 end
