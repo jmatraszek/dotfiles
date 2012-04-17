5142edae086c43abbaacf1e12087e29044461f14
Jakub Matraszek <qba@hatepunxlap.(none)>
2012-04-17 22:36:30 +0200

    removed muttrc.example from install.rb

--- install.rb
+++ install.rb
@@ -7,7 +7,7 @@
 require 'fileutils'
 include FileUtils
 
-["gitconfig", "muttrc"].each { |file| cp "#{file}.example", file }
+["gitconfig"].each { |file| cp "#{file}.example", file }
 
 ["ackrc", "bash_aliases", "bashrc", "gitconfig", "irbrc", "irssi", "mailcap", "mutt", "muttrc", "tigrc", "tmux.conf"].each do |file|
   ln_sf file, "~/.#{file}"
