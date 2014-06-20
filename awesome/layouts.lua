cb1d88886dbf83092be51d067d55aedff64ce457
Jakub Matraszek <jakub.matraszek@up-next.com>
2014-06-20 11:14:22 +0200

    Chage default layout for some tags

--- awesome/layouts.lua
+++ awesome/layouts.lua
@@ -3,6 +3,7 @@ layouts =
 {
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
+    awful.layout.suit.tile.right,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
     awful.layout.suit.fair,
