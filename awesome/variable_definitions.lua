336f7008a8c2119d2c102e6b340f054856e5c79b
Jakub Matraszek <jakub.matraszek@up-next.com>
2014-06-27 08:48:59 +0200

    Use modified sky theme to fix issue with some wrong colors

--- awesome/variable_definitions.lua
+++ awesome/variable_definitions.lua
@@ -1,19 +1,9 @@
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
+
 config_dir = awful.util.getdir("config")
-beautiful.init(config_dir .. "/current_theme/theme.lua")
-
-beautiful.border_focus  = "#ff0000"
-beautiful.border_normal = "#c0c0c0"
-beautiful.cpu_icon      = config_dir .. "/icons/cpu.png"
-beautiful.mem_icon      = config_dir .. "/icons/mem.png"
-beautiful.fs_icon       = config_dir .. "/icons/fs.png"
-beautiful.netup_icon    = config_dir .. "/icons/net_up.png"
-beautiful.netdown_icon  = config_dir .. "/icons/net_down.png"
-beautiful.bg_normal     = "#5b7fa6"
-beautiful.bg_systray    = beautiful.bg_normal
-
-
+-- beautiful.init(config_dir .. "/current_theme/theme.lua")
+beautiful.init(config_dir .. "/themes/jsky/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "konsole"
