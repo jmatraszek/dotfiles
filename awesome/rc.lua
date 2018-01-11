d2e9f6464392b5322c92319fe4a92c84c1515c2d
Jakub Matraszek <jakub.matraszek@gmail.com>
2018-01-11 22:49:37 +0100

    Fix autostart

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -589,9 +589,12 @@ naughty.config.notify_callback = function(args)
 end
 --
 
+runonce.run("cmst --minimized -i Arc")
+runonce.run("qxkb")
 runonce.run("redshift-gtk -l 52.7:21.6 -t 5700:4300 -g 0.8 -m randr")
 runonce.run("udiskie -2 --smart-tray --notify --no-automount")
 runonce.run("clipit")
 runonce.run("pasystray --notify=sink_default")
+runonce.run("qopenvpn")
 
 require("run_local")
