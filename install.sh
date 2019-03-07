7418240ec83c5239297a3bc07e7ef3ae9e822752
Jakub Matraszek <jakub.matraszek@gmail.com>
2019-03-07 11:30:43 +0100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore ~/.gitignore
+ln -s ~/.dotfiles/tigrc ~/.tigrc
+ln -s ~/.dotfiles/psqlrc ~/.psqlrc
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/xinitrc ~/.xinitrc
+ln -s ~/.dotfiles/pqivrc ~/.pqivrc
+
+
+mkdir -p ~/.cache/awesome
+mkdir -p ~/.config/
+
+ln -s ~/.dotfiles/awesome ~/.config/
