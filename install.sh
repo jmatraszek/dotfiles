4fe20d0ea4e394efd871daaa38b5e9fcfbf40161
Jakub Matraszek <jakub.matraszek@gmail.com>
2019-04-12 17:41:43 +0200

    New tmux config

--- install.sh
+++ install.sh
@@ -7,12 +7,13 @@ ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/tigrc ~/.tigrc
 ln -s ~/.dotfiles/psqlrc ~/.psqlrc
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/xinitrc ~/.xinitrc
 ln -s ~/.dotfiles/pqivrc ~/.pqivrc
 ln -s ~/.dotfiles/bat ~/.config/bat
 ln -s ~/.dotfiles/mutt ~/.config/mutt
 ln -s ~/.dotfiles/mailcap ~/.mailcap
+ln -s ~/.dotfiles/tmux ~/.config/tmux
+ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 
 mkdir -p ~/.cache/awesome
